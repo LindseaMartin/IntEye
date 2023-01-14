2fbfa30c4557d0c8060744299e160cce2341aeb9
LindseaMartin <lindseamartin@gmail.com>
2023-01-14 14:33:49 -0700

    first commit, only sign button

---
+++ src/Components/Navigation/navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav style={{ display: "flex", jusifyContent: "flex-end" }}>
+      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
+  );
+};
