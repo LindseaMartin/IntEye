a5dfd1b9e000e51b10cc92b3fef580b0664bc67e
LindseaMartin <lindseamartin@gmail.com>
2023-01-16 15:39:32 -0700

    Temporary front end done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import ParticlesBg from 'particles-bg'
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/logo/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
--- src/Components/logo/Logo.js
+++ src/Components/logo/Logo.js
@@ -4,10 +4,10 @@ import logo from "./logo.png";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0">
+    <div className="ma4 mt0" style= {{width:'200px'}}>
       <Tilt>
-        <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
-          <img src={logo} alt='logo'></img>
+        <div style={{ height: '200px', width: '200px', backgroundColor: 'darkgreen' }}>
+          <img style={{paddingTop:'40px'}} src={logo} alt='logo'></img>
         </div>
       </Tilt>
       <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/4wAPK6Ah5gOX/illuminati">Illuminati</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
